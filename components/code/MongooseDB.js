e9cb40635b9b3ae461d92b6b1a2a475e888e3537
aslamzaman <37665857+aslamzaman@users.noreply.github.com>
2024-11-27 01:06:01 +0600

    Update MongooseDB.js

--- components/code/MongooseDB.js
+++ components/code/MongooseDB.js
@@ -2,7 +2,8 @@
 const MongooseDB = () => {
 
 
-    const str = `import mongoose from "mongoose";
+    const str = `// npm install mongoose@8.6.4
+import mongoose from "mongoose";
 
 export const Connect = async () => {
 	try {
