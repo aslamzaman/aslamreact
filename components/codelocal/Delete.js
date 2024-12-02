0da98c4ab5b6021cc2c296a71cda69c3b6fd28bd
aslamzaman <37665857+aslamzaman@users.noreply.github.com>
2024-12-02 23:29:09 +0600

    Update Delete.js

--- components/codelocal/Delete.js
+++ components/codelocal/Delete.js
@@ -50,7 +50,7 @@ const Delete = ({ message, id, data }) => {
         <>
             {show && (
                 <div className="fixed inset-0 py-16 bg-black bg-opacity-30 backdrop-blur-sm z-10 overflow-auto">
-                    <div className="w-11/12 md:w-1/2 mx-auto mb-10 bg-white border-2 border-gray-300 rounded-md shadow-md duration-300">
+                    <div className="w-full md:w-3/4 mx-auto mb-10 bg-white border-2 border-gray-300 rounded-md shadow-md duration-300">
                         <div className="px-6 md:px-6 py-2 flex justify-between items-center border-b border-gray-300">
                             <h1 className="text-xl font-bold text-blue-600">Delete Existing Data</h1>
                             <button onClick={closeDeleteForm} className="w-8 h-8 p-0.5 bg-gray-50 hover:bg-gray-300 rounded-md transition duration-500">
