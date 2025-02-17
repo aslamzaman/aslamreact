de3b267ffcb2cac4d05dda933dbe3193b376c3c6
aslam <aslamcmes@gmail.com>
2025-02-17 17:20:03 +0600

    Unit bounus

--- app/unitbonus/page.js
+++ app/unitbonus/page.js
@@ -1,15 +1,15 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import Add from "@/components/unitsalary/Add";
-import Edit from "@/components/unitsalary/Edit";
-import Delete from "@/components/unitsalary/Delete";
-import Print from "@/components/unitsalary/Print";
+import Add from "@/components/unitbonus/Add";
+import Edit from "@/components/unitbonus/Edit";
+import Delete from "@/components/unitbonus/Delete";
+import Print from "@/components/unitbonus/Print";
 import { getDataFromFirebase } from "@/lib/firebaseFunction";
 import { numberWithComma, sortArray } from "@/lib/utils";
 
 
-const Unitsalary = () => {
-    const [unitsalarys, setUnitsalarys] = useState([]);
+const Unitbonus = () => {
+    const [unitbonuss, setUnitbonuss] = useState([]);
     const [msg, setMsg] = useState("Data ready");
     const [waitMsg, setWaitMsg] = useState("");
 
@@ -20,13 +20,12 @@ const Unitsalary = () => {
         const loadData = async () => {
             setWaitMsg('Please Wait...');
             try {
-                const [unitsalaryResponse, staffResponse, unitResponse, postResponse] = await Promise.all([
-                    getDataFromFirebase("unitsalary"),
+                const [unitbonusResponse, staffResponse, unitResponse, postResponse] = await Promise.all([
+                    getDataFromFirebase("unitbonus"),
                     getDataFromFirebase("staff"),
                     getDataFromFirebase("unit"),
                     getDataFromFirebase("post")
                 ]);
-console.log(unitsalaryResponse);
                 const staffs = staffResponse.map(staff => {
                     const getUnit = unitResponse.find(unit => unit.id === staff.unitId);
                     const getPost = postResponse.find(post => post.id === staff.postId);
@@ -37,18 +36,18 @@ console.log(unitsalaryResponse);
                     }
                 })
 
-                const data = unitsalaryResponse.map(salary => {
-                    const staff = staffs.find(staff => staff.id === salary.staffId);
+                const data = unitbonusResponse.map(bonus => {
+                    const staff = staffs.find(staff => staff.id === bonus.staffId);
                     return {
-                        ...salary,
+                        ...bonus,
                         staff
                     }
                 })
-                const sortData = data.sort((a, b)=>sortArray( new Date(a.createdAt),  new Date(b.createdAt)));
-                console.log(sortData);
-                setUnitsalarys(sortData);
-                const totalSalary = data.reduce((t, c) => t + (parseFloat(eval(c.arear)) + parseFloat(c.sal1) + parseFloat(c.sal2)), 0);
-                setTotal(totalSalary);
+                const sortData = data.sort((a, b) => sortArray(new Date(a.createdAt), new Date(b.createdAt)));
+                console.log({ sortData });
+                setUnitbonuss(sortData);
+                const totalBounus = data.reduce((t, c) => t + parseFloat(c.bonus), 0);
+                setTotal(totalBounus);
 
                 setWaitMsg('');
             } catch (error) {
@@ -65,10 +64,11 @@ console.log(unitsalaryResponse);
     }
 
 
+
     return (
         <>
             <div className="w-full mb-3 mt-8">
-                <h1 className="w-full text-xl lg:text-4xl font-bold text-center text-blue-700">Unit Salary: {numberWithComma(parseFloat(total))}/-</h1>
+                <h1 className="w-full text-xl lg:text-4xl font-bold text-center text-blue-700">Unit CTNG Bonus: {numberWithComma(parseFloat(total))}/-</h1>
                 <p className="w-full text-center text-blue-300">&nbsp;{waitMsg}&nbsp;</p>
             </div>
             <div className="px-4 lg:px-6">
@@ -80,38 +80,34 @@ console.log(unitsalaryResponse);
                                 <th className="text-center border-b border-gray-200 px-4 py-2">SL</th>
                                 <th className="text-start border-b border-gray-200 px-4 py-2">Staff</th>
                                 <th className="text-center border-b border-gray-200 px-4 py-2">Unit</th>
-                                <th className="text-center border-b border-gray-200 px-4 py-2">Arear</th>
-                                <th className="text-center border-b border-gray-200 px-4 py-2">Sal1</th>
-                                <th className="text-center border-b border-gray-200 px-4 py-2">Sal2</th>
+                                <th className="text-center border-b border-gray-200 px-4 py-2">Bonus</th>
                                 <th className="text-center border-b border-gray-200 px-4 py-2">Remarks</th>
                                 <th className="w-[100px] font-normal">
                                     <div className="w-full flex justify-end space-x-1 py-0.5">
                                         <Add message={messageHandler} />
-                                        <Print data={unitsalarys} message={messageHandler} />
+                                        <Print data={unitbonuss} message={messageHandler} />
                                     </div>
                                 </th>
                             </tr>
                         </thead>
                         <tbody>
-                            {unitsalarys.length ? (
-                                unitsalarys.map((unitsalary, i) => (
-                                    <tr className="border-b border-gray-200 hover:bg-gray-100" key={unitsalary.id}>
+                            {unitbonuss.length ? (
+                                unitbonuss.map((bonus, i) => (
+                                    <tr className="border-b border-gray-200 hover:bg-gray-100" key={bonus.id}>
                                         <td className="text-center py-2 px-4">{i + 1}</td>
-                                        <td className="text-start py-2 px-4"><span className="font-bold">Employee Id: {unitsalary.staff.empId}</span><br />{unitsalary.staff.nmEn} ({unitsalary.staff.post.nmEn})</td>
-                                        <td className="text-center py-2 px-4">{unitsalary.staff.unit.nmEn}</td>
-                                        <td className="text-center py-2 px-4">{unitsalary.arear}</td>
-                                        <td className="text-center py-2 px-4">{unitsalary.sal1}</td>
-                                        <td className="text-center py-2 px-4">{unitsalary.sal2}</td>
-                                        <td className="text-center py-2 px-4 font-sutonnyN">{unitsalary.remarks}</td>
+                                        <td className="text-start py-2 px-4"><span className="font-bold">Employee Id: {bonus.staff.empId}</span><br />{bonus.staff.nmEn} ({bonus.staff.post.nmEn})</td>
+                                        <td className="text-center py-2 px-4">{bonus.staff.unit.nmEn}</td>
+                                        <td className="text-center py-2 px-4">{bonus.bonus}</td>
+                                        <td className="text-center py-2 px-4 font-sutonnyN">{bonus.remarks}</td>
                                         <td className="h-8 flex justify-end items-center space-x-1 mt-1 mr-2">
-                                            <Edit message={messageHandler} id={unitsalary.id} data={unitsalary} />
-                                            <Delete message={messageHandler} id={unitsalary.id} data={unitsalary} />
+                                            <Edit message={messageHandler} id={bonus.id} data={bonus} />
+                                            <Delete message={messageHandler} id={bonus.id} data={bonus} />
                                         </td>
                                     </tr>
                                 ))
                             ) : (
                                 <tr>
-                                    <td colSpan={7} className="text-center py-10 px-4">
+                                    <td colSpan={6} className="text-center py-10 px-4">
                                         Data not available.
                                     </td>
                                 </tr>
@@ -125,6 +121,6 @@ console.log(unitsalaryResponse);
 
 };
 
-export default Unitsalary;
+export default Unitbonus;
 
 
