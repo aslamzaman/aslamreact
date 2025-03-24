5cc765782d397b4e4001af0fd571aaaf833a3919
Aslam Zaman <aslamcmes@gmail.com>
2025-03-24 08:39:21 -0700

    dashboard chance

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -8,7 +8,7 @@ const DashboardPage = () => {
 
     return (
         <>
-            <h1 className="w-full py-4 text-xl lg:text-3xl font-bold text-center text-blue-700">Dashboard</h1>
+            <h1 className="w-full py-4 text-xl lg:text-3xl font-bold text-center text-blue-700">Welcome</h1>
             <div className="w-full lg:w-3/4 mx-auto border-2 shadow-md rounded-md overflow-auto">
                 <Image src={dashboard} alt="dashboard" width={399.1} height={261.8} priority={true} className="w-full h-auto mx-auto" />
             </div>
