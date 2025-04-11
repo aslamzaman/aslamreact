fa1c08a5e85991dde6ae14990d9b07d84c639790
aslamzaman <37665857+aslamzaman@users.noreply.github.com>
2025-04-12 01:30:28 +0600

    Update Add.js

--- components/codelocal/Add.js
+++ components/codelocal/Add.js
@@ -80,7 +80,7 @@ ${objectText}
     return (
         <>
             {show && (
-                <div className="fixed inset-0 px-2 py-16 bg-black bg-opacity-30 backdrop-blur-sm z-10 overflow-auto">
+                <div className="fixed inset-0 px-2 py-16 bg-gray-500/50 z-10 overflow-auto">
                     <div className="w-full md:w-[500px] lg:w-[800px] mx-auto mb-10 bg-white border-2 border-gray-300 rounded-md shadow-md duration-300">
                         <div className="px-6 md:px-6 py-2 flex justify-between items-center border-b border-gray-300">
                             <h1 className="text-xl font-bold text-blue-600">Add New Data</h1>
