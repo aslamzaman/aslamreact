e96df6e078bf6a54251b7e7263d9813fce607440
aslamzaman <37665857+aslamzaman@users.noreply.github.com>
2025-04-12 01:29:14 +0600

    Update Delete.js

--- components/codemodels/Delete.js
+++ components/codemodels/Delete.js
@@ -64,7 +64,7 @@ const Delete = ({ message, id, data }) => {
         <>
             {busy ? <LoadingDot message="Please wait" /> : null}
             {show && (
-                <div className="fixed left-0 top-[60px] right-0 bottom-0 p-4 bg-black bg-opacity-30 backdrop-blur-sm z-10 overflow-auto">
+                <div className="fixed left-0 top-[60px] right-0 bottom-0 p-4 bg-gray-500/50 z-10 overflow-auto">
                     <div className="w-full lg:w-3/4 mx-auto my-10 bg-white border-2 border-gray-300 rounded-md shadow-md duration-500">
                         <div className="px-4 md:px-6 py-2 flex justify-between items-center border-b border-gray-300">
                             <h1 className="text-xl font-bold text-blue-600">Delete Existing Data</h1>
